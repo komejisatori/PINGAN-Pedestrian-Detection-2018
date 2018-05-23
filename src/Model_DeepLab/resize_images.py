e8c8fb71195aa74a898a2bc117ad323ad826dd03
WuZhuoran <munoliver007@gmail.com>
2018-05-23 16:31:20 -0700

    fix: use hard image resize

--- src/Model_DeepLab/resize_images.py
+++ src/Model_DeepLab/resize_images.py
@@ -1,14 +1,17 @@
 import cv2
 import os
 
-directory = os.fsencode('/home/oliver/segmentation/resize_data')
+source = '/home/oliver/segmentation/resize_data'
 
-for file in os.listdir(directory):
-    filename = os.fsdecode(file)
-    W = 1480.
-    oriimg = cv2.imread(filename)
-    height, width, depth = oriimg.shape
-    imgScale = W / width
-    newX, newY = oriimg.shape[1] * imgScale, oriimg.shape[0] * imgScale
-    newimg = cv2.resize(oriimg, (int(newX), int(newY)))
-    cv2.imwrite('/home/oliver/segmentation/after_resize/' + filename, newimg)
+for root, dirs, filenames in os.walk(source):
+    for f in filenames:
+        fullpath = os.path.join(source, f)
+        W = 1664
+        H = 825
+        oriimg = cv2.imread(fullpath)
+        # height, width, depth = oriimg.shape
+        # imgScale = W / width
+        # newX, newY = oriimg.shape[1] * imgScale, oriimg.shape[0] * imgScale
+        newimg = cv2.resize(oriimg, (W, H))
+        cv2.imwrite('/home/oliver/segmentation/after_resize/' + f, newimg)
+        # cv2.imwrite('../Model_HOG_SVM/model/test_image/' + f + 'New.jpg', newimg)
